3f8afeb1809a5bf8246ed97c92af8188987c02ca
Zhang, Jizhao <jizhao.zhang@intel.com>
2018-08-13 09:47:16 +0800

    implement several import functions
    Change-Id: Idb72a8763dcd4685efef83cb08622b98b8a8f9d3

--- runtime/vmcore_wasm/wasm-native.c
+++ runtime/vmcore_wasm/wasm-native.c
@@ -22,10 +22,19 @@
  * such intellectual property rights must be express and approved by
  * Intel in writing.
  */
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE /* for O_DIRECT */
+#endif
 
 #include "wasm-native.h"
 #include "wasm-runtime.h"
+#include "bh_memory.h"
 
+#include <sys/ioctl.h>
+#include <sys/uio.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define MEMORY(self) (self->vm_instance->module->default_memory)
 #define MEMORY_BASE(self) (MEMORY(self)->base_addr)
@@ -33,19 +42,30 @@
 static void
 _getc_wrapper(WASMThread *self, uint32 *args)
 {
-  /* TODO */
+  int32 file = args[0];
+  int32 ret = getc((void*)file);
+  *args = ret;
 }
 
 static void
 _ungetc_wrapper(WASMThread *self, uint32 *args)
 {
-  /* TODO */
+  int32 character = args[0];
+  int32 file = args[1];
+  int32 ret = ungetc(character, (void*)file);
+  *args = ret;
 }
 
 static void
 _fread_wrapper(WASMThread *self, uint32 *args)
 {
-  /* TODO */
+  uint8 *memory_base = MEMORY_BASE(self);
+  int32 off = args[0];
+  int32 size = args[1];
+  int32 nmemb = args[2];
+  int32 file = args[3];
+  int32 ret = fread(memory_base + off, size, nmemb, (void*)file);
+  *args = ret;
 }
 
 static void
@@ -63,13 +83,117 @@ _fwrite_wrapper(WASMThread *self, uint32 *args)
 static void
 _fputc_wrapper(WASMThread *self, uint32 *args)
 {
-  /* TODO */
+  int32 character = args[0];
+  int32 file = args[1];
+  int32 ret = fputc(character, (void*)file);
+  *args = ret;
 }
 
 static void
 _fflush_wrapper(WASMThread *self, uint32 *args)
 {
-  /* TODO */
+  int32 file = args[0];
+  int32 ret = fflush((void*)file);
+  *args = ret;
+}
+
+static void
+abort_wrapper(WASMThread *self, uint32 *args)
+{
+  int32 code = args[0];
+  printf("env.abort(%i)\n",code);
+  abort();
+}
+
+static void
+___syscall140_wrapper(WASMThread *self, uint32 *args)
+{
+  /* llseek */
+  /* could not call _llseek since it's not exposed in glibc, and syscall is also impossible to be called, so use lseek64 instead. */
+  /*
+  int64 *p = (int64*)args + sizeof(int32);
+  int32 fd = p[0];
+  int64 off_high = p[1];
+  int64 off_low = p[2];
+  int64 *result = &p[3];
+  int32 whence = p[4];
+  int32 ret = syscall(SYS_llseek, fd, off_high, off_low, result, whence);
+
+  *(int64*)args = *result;
+
+  if (ret != 0) {
+    printf("___syscall140_wrapper execute failed.\n");
+    abort();
+  }
+  */
+
+  int64 *p = (int64*)args + sizeof(int32);
+  int32 fd = p[0];
+  int64 off_high = p[1];
+  int64 off_low = p[2];
+  int32 whence = p[4];
+  int64 off = ((off_high << 32) & (0xffffffff00000000)) | off_low;
+  int32 ret = lseek64(fd, off, whence);
+
+  *args = ret;
+}
+
+static void
+___syscall146_wrapper(WASMThread *self, uint32 *args)
+{
+  /* writev */
+  uint32 i, ptr, len, count;
+  uint8 *memory_base = MEMORY_BASE(self);
+  int32 fileno = args[1];
+  int32 iov = args[2];
+  uint32 iovcnt = args[3];
+  struct iovec *native_iovec;
+
+  if(!(native_iovec = bh_malloc(sizeof(struct iovec) * iovcnt))) {
+    printf("alloc iovec failed in native function lookup.\n");
+    abort();
+  }
+
+  for (i = 0; i < iovcnt; i++) {
+    ptr = iov + i * 8;
+    len = iov + i * 8 + 4;
+    native_iovec[i].iov_base = memory_base + ptr;
+    native_iovec[i].iov_len = len;
+  }
+
+  count = writev(fileno, native_iovec, iovcnt);
+  *args = count;
+}
+
+static void
+___syscall54_wrapper(WASMThread *self, uint32 *args)
+{
+  /* ioctl */
+  /* TODO: might need support VA args for ioctl() in the future */
+  int32 fd = args[1];
+  int32 cmd = args[2];
+  int32 ret = ioctl(fd, cmd);
+  *args = ret;
+}
+
+static void
+___syscall6_wrapper(WASMThread *self, uint32 *args)
+{
+  /* close */
+  int32 fd = args[1];
+  int32 ret = close(fd);
+  *args = ret;
+}
+
+static void
+_emscripten_memcpy_big_wrapper(WASMThread *self, uint32 *args)
+{
+  uint8 *memory_base = MEMORY_BASE(self);
+  int32 off_dest = args[0];
+  int32 off_src = args[1];
+  int32 len = args[2];
+  memcpy(memory_base + off_dest, memory_base + off_src, len);
+  *args = off_dest;
 }
 
 /* TODO: add function parameter/result types check */
@@ -88,7 +212,13 @@ static WASMNativeFuncDef native_func_defs[] = {
   REG_NATIVE_FUNC(env, _fread),
   REG_NATIVE_FUNC(env, _fwrite),
   REG_NATIVE_FUNC(env, _fputc),
-  REG_NATIVE_FUNC(env, _fflush)
+  REG_NATIVE_FUNC(env, _fflush),
+  REG_NATIVE_FUNC(env, abort),
+  REG_NATIVE_FUNC(env, ___syscall140),
+  REG_NATIVE_FUNC(env, ___syscall146),
+  REG_NATIVE_FUNC(env, ___syscall54),
+  REG_NATIVE_FUNC(env, ___syscall6),
+  REG_NATIVE_FUNC(env, _emscripten_memcpy_big)
 };
 
 void*
@@ -124,11 +254,11 @@ typedef struct WASMNativeGlobalDef {
 } WASMNativeGlobalDef;
 
 static WASMNativeGlobalDef native_global_defs[] = {
-  { "env", "_STACKTOP", (uintptr_t)64 * NumBytesPerPage },
-  { "env", "_STACK_MAX", 128 * NumBytesPerPage },
-  { "env", "_ABORT", 0 },
-  { "env", "_memoryBase", 1024 },
-  { "env", "_tableBase", 0 }
+  { "env", "STACKTOP", (uintptr_t)64 * NumBytesPerPage },
+  { "env", "STACK_MAX", 128 * NumBytesPerPage },
+  { "env", "ABORT", 0 },
+  { "env", "memoryBase", 1024 },
+  { "env", "tableBase", 0 }
 };
 
 void*
