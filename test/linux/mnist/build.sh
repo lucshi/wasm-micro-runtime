e2410ba1cc4316fe5cc03c9a21a2074e184caf81
Zhang, Jizhao <jizhao.zhang@intel.com>
2018-10-23 14:50:57 +0800

    a simple mnist sample from github, extremely slow to train/test huge data and might have bugs, but can be run by our vm.
    Change-Id: I1c003d3a8540eafca9b55930e8251319bca5d532

---
+++ test/linux/mnist/build.sh
@@ -0,0 +1,1 @@
+emcc -g -Os -s WASM=1 -s SIDE_MODULE=1 *c -o ./simplenet.wasm
---
+++ test/linux/mnist/netdebugger.c
@@ -0,0 +1,12 @@
+#include "netdebugger.h"
+#include "simplenet.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void printPath()
+{
+}
+
+void printNet(SimpleNet *net)
+{
+}
