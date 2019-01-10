85e3a19add2bd850e9a9318a001bdc2f6a55ec8a
Huang, Wenyong <wenyong.huang@intel.com>
2019-01-10 11:33:06 +0800

    Remove tool iec2wasm
    Change-Id: I32db73b1b9f1963109d704f4b883c1e38c076871

--- tools/iec2wasm/trans_for.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-from ctypes import *
-from global_vars import *
-from pou import *
-from operand import *
-
-# translate for statements, return binaryen_ref
-def translateForLoop(module, pou_name, ast_node):
-  return None
